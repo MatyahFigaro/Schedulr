6f336dd7fd19615342ed51d1eb14366dbf257984
Haytam JEBBARI <hjebbari@lefigaro.fr>
2026-02-08 08:53:08 +0000

    feat(db): implement Prisma client and update exports in index file

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,16 +1,2 @@
-import { PrismaClient } from '../generated/prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
-
-const globalForPrisma = globalThis as unknown as { prisma: PrismaPg };
-
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter,
-  });
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+export { prisma } from './client'; // exports instance of prisma
+export * from '../generated/prisma/client'; // exports generated types from prisma
